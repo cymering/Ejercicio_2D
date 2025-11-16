98706fe1bfcf538e3a896f7406cb94e5444b890a
cymering <cymering@gmail.com>
2025-11-16 13:59:27 -0300

    mouse position added

---
+++ Assets/Scripts/Ball_actions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_actions : MonoBehaviour
+{
+    [SerializeField] Transform transformDelObjeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transformDelObjeto.Translate(Vector3.down * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Click");
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.WorldToScreenPoint(transformDelObjeto.position).z;
+            transformDelObjeto.position = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+    }
+}
